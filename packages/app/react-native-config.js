d50eb56e3ca2dc9eaa6333205443c5b7a4b452fd
7patricia <patricia.barros@uphold.com>
2019-12-16 15:21:50 +0000

    Re-add react native config file

---
+++ packages/app/react-native-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dependency: {
+      platforms: {
+        android: {
+          packageImportPath: 'import io.invertase.firebase.app.ReactNativeFirebaseAppPackage;',
+        }
+      }
+    }
+  };
+  
